8a3ab0a99f796fd9e0aad563f6e215a131f52140
vantutrieu97 <vantutrieu97@gmail.com>
2020-05-11 11:49:14 +0700

    Add new screen

---
+++ authenticationlibrary/src/main/java/test/itsol/authenticationlibrary/TutotialScreen.kt
@@ -0,0 +1,12 @@
+package test.itsol.authenticationlibrary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TutotialScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutotial_screen)
+    }
+}
