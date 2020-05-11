f729adf8b5ae47c09f4b1684e801dc7b6b8d866b
vantutrieu97 <vantutrieu97@gmail.com>
2020-05-11 11:24:04 +0700

    create static function in Kotlin

--- authenticationlibrary/src/main/java/AuthenticationTest.kt
+++ authenticationlibrary/src/main/java/AuthenticationTest.kt
@@ -2,7 +2,13 @@ import android.content.Context
 import android.widget.Toast
 
 class AuthenticationTest {
-    public fun ToastTest(context: Context, message: String) {
+    companion object {
+        fun ToastTest2(context: Context, message: String) {
+            Toast.makeText(context, "This is message: $message", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun ToastTest(context: Context, message: String) {
         Toast.makeText(context, "This is message: $message", Toast.LENGTH_SHORT).show()
     }
 }
